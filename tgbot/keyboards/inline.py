81fd4328bcf1ed5c61ee31d633232e6b15401f31
macbook <ikamil522@gmail.com>
2023-02-13 17:49:29 +0300

    улучшения кнопок

--- tgbot/keyboards/inline.py
+++ tgbot/keyboards/inline.py
@@ -1,27 +1,30 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-button1 = InlineKeyboardButton(callback_data='1', text='1')
-button2 = InlineKeyboardButton(callback_data='2', text='2')
-button3 = InlineKeyboardButton(callback_data='3', text='3')
 
-markup = [[button1], [button2], [button3]]
-keyboard = InlineKeyboardMarkup(inline_keyboard=markup)
+menu_buttons = [
 
-stiralki = InlineKeyboardButton(callback_data='stiralka', text='Стиралки')
-sushilki = InlineKeyboardButton(callback_data='sushilka', text='Сушилки')
-instruc = InlineKeyboardButton(callback_data='instr', text='Инструкция')
+        [InlineKeyboardButton(callback_data='stiralka', text='Стиралки')],
+        [InlineKeyboardButton(callback_data='sushilka', text='Сушилки')],
+        [InlineKeyboardButton(callback_data='instr', text='Инструкция')]
+]
+menu_keyboard = InlineKeyboardMarkup(inline_keyboard=menu_buttons)
 
-menu_markup = [[stiralki], [sushilki], [instruc]]
-menu_keyboard = InlineKeyboardMarkup(inline_keyboard=menu_markup)
+floor_buttons = [
+    [
+        InlineKeyboardButton(callback_data='1', text='1'),
+        InlineKeyboardButton(callback_data='2', text='2'),
+        InlineKeyboardButton(callback_data='3', text='3')
+    ],
+    [InlineKeyboardButton(text="Назад", callback_data="back")]
+]
+floor_keyboard = InlineKeyboardMarkup(inline_keyboard=floor_buttons)
 
-#inline клавиатура с 3 кнопками в одном ряду
-
-mash1 = InlineKeyboardButton(callback_data='mash1', text='🟢')
-mash2 = InlineKeyboardButton(callback_data='mash2', text='🟢')
-mash3 = InlineKeyboardButton(callback_data='mash3', text='🟢')
-
-#mash_keybord для их отображения
-
-mash_markup = [[mash1], [mash2], [mash3]]
-mash_keyboard = InlineKeyboardMarkup(inline_keyboard=mash_markup)
-
+mash_buttons = [
+    [
+        InlineKeyboardButton(callback_data='mash1', text='🟢'),
+        InlineKeyboardButton(callback_data='mash2', text='🟢'),
+        InlineKeyboardButton(callback_data='mash3', text='🟢')
+    ],
+    [InlineKeyboardButton(text="Назад", callback_data="back")]
+]
+mash_keyboard = InlineKeyboardMarkup(inline_keyboard=mash_buttons)
