ee863aeb9a471f431be7371e0e88b7db2ea1601e
Klotz Galtry <klotzgal@pr-i4.kzn.21-school.ru>
2023-02-19 21:24:37 +0300

    del_json

--- tgbot/users_logging/logging.py
+++ tgbot/users_logging/logging.py
@@ -5,10 +5,10 @@ import os
 # import asyncio
 
 
-def create_new_json(megapivo, key):
+def create_new_json():
     today = datetime.datetime.now().strftime("%Y-%m-%d")
     with open(f'{today}_log.json', 'w') as f:
-        json.dump({key: megapivo}, f, indent=4)
+        json.dump({}, f, indent=4)
 
 
 def write_to_json(user_id, floor, car_number, mode, time, finish_time, progress):
@@ -17,21 +17,36 @@ def write_to_json(user_id, floor, car_number, mode, time, finish_time, progress)
     #     data = json.load(f)
     data = {'floor': floor, 'car_number': car_number, 'mode': mode,
             'time': time, 'finish_time': finish_time, 'progress': progress}
+    # print(data)
     key = floor + '.' + car_number
-    if check_file(data, key):
-        with open(f'{today}_log.json', 'r') as f:
-            pepe = json.load(f)
-        pepe[key] = data
-        with open(f'{today}_log.json', 'w') as f:
-            json.dump(pepe, f, indent=4)
+    check_file(data, key)
+    with open(f'{today}_log.json', 'r') as f:
+        pepe = json.load(f)
+    pepe[key] = data
+    with open(f'{today}_log.json', 'w') as f:
+        json.dump(pepe, f, indent=4)
 
 
 def check_file(megapivo, key):
     today = datetime.datetime.now().strftime("%Y-%m-%d")
     if f'{today}_log.json' not in os.listdir():
-        create_new_json(megapivo, key)
+        create_new_json()
     else:
         return 1
+
+
+def delete_json():
+    today = datetime.datetime.now().strftime("%Y-%m-%d")
+    with open(f'{today}_log.json', 'r') as f:
+        pepe = json.load(f)
+    del_list = []
+    for key in pepe:
+        if pepe[key]['finish_time'] == datetime.datetime.now().strftime("%d-%m-%Y %H:%M"):
+            del_list.append(key)
+    for elem in del_list:
+        pepe.pop(elem)
+    with open(f'{today}_log.json', 'w') as f:
+        json.dump(pepe, f, indent=4)
 
 
 def main():
@@ -39,14 +54,19 @@ def main():
     user_id = '123'
     floor = '1'
     mode = 'mode'
-    time = 'time'
-    finish_time = 'finish_time'
+    time = datetime.datetime.now().strftime("%d-%m-%Y %H:%M")
+    # print(type(time))
+    finish_time1 = (datetime.datetime.now() +
+                    datetime.timedelta(minutes=1)).strftime("%d-%m-%Y %H:%M")
+    finish_time2 = (datetime.datetime.now() +
+                    datetime.timedelta(minutes=2)).strftime("%d-%m-%Y %H:%M")
     progress = 'progress'
-    # create_new_json()
     write_to_json(user_id, floor, '1', mode,
-                  time, finish_time, progress)
+                  time, finish_time1, progress)
     write_to_json(user_id, floor, '2', mode,
-                  time, finish_time, progress)
+                  time, finish_time2, progress)
+
+    delete_json()
 
 
 if __name__ == '__main__':
